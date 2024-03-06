1d20bd72ce183cecb64ee5786c3478e8973758f4
Vikas Kumar <115536827+Vikas-aetna@users.noreply.github.com>
2024-03-06 14:40:16 +0530

    Initial Project Setup

---
+++ GitTestingSample/GitTestingSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestingSample
+//
+//  Created by Vikas Kumar on 06/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
